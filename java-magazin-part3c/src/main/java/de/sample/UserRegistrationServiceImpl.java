44bbd63d38da00e8910938b1bed288f3ac5f4dc0
Marcel Birkner <marcel.birkner@gmail.com>
2011-03-26 19:23:12 +0100

    Added logging to Service Impl

--- java-magazin-part3c/src/main/java/de/sample/UserRegistrationServiceImpl.java
+++ java-magazin-part3c/src/main/java/de/sample/UserRegistrationServiceImpl.java
@@ -2,7 +2,13 @@ package de.sample;
 
 import java.util.Random;
 
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+
 public class UserRegistrationServiceImpl implements UserRegistrationService {
+	
+	@Inject Logger log;
 	
 	private Random random = new Random();
 	
@@ -10,6 +16,7 @@ public class UserRegistrationServiceImpl implements UserRegistrationService {
 		User user = new User();
 		int randomInteger = random.nextInt( 2 );
 		if( randomInteger == 0 ) {
+			log.debug("set spam email address");
 			user.setEmail("spam@mail.com");
 		}
 		return user;
